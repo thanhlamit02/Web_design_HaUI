58e22e3c9f1a46866fdea018f3e88511420bdad1
Nguyễn Thành Lâm <laotuong2002@gmail.com>
2022-11-04 01:11:27 +0700

    JS_Exam_HaUI

---
+++ JS_Exam/De 3/script.js
@@ -0,0 +1,26 @@
+function myFunction() {
+    const num = document.getElementById('number').value;
+    if (num == "") {
+        alert("Không được để trống!");
+    } else {
+        let sum = 0;
+        console.log(num);
+
+        for (let i = 6; i <= num; i++) {
+            if (check(i)) {
+                sum += i;
+            }
+        }
+        document.getElementById('result').value = sum;
+    }
+}
+
+function check(n) {
+    let sum = 0;
+    for (let i = 1; i <= n / 2; i++) {
+        if (n % i == 0)
+            sum += i;
+    }
+    if (sum == n) return true;
+    return false;
+}
